b88f2d52d98c2d55f9e556e9598935503117c767
Leonardo Navarro <maikndawer@gmail.com>
2015-06-24 16:44:51 -0300

    se mejora el rendimiento de cada tirada de dados

--- js/dados.js
+++ js/dados.js
@@ -5,21 +5,43 @@
 var CubileteMagico = function(){
 
 	this.cantidadDados = 1;
-
+	
 
 	this.init = function(cantidad){
-		if (!isNaN(cantidad))
+		if (!isNaN(cantidad)){
 			this.cantidadDados = cantidad;
-		else
-			alert('El valor ingresado no es un numero válido');
+		}else
+			alert('Debe ingresar un valor mayor a 1');
 	}
 	
 	this.tirarDados = function(){
-		var d = 0;
-		for (var i = 0; i < this.cantidadDados; i++) {
-			d = Math.floor((Math.random() * 6) + 1);
-
+		var d = 0,
+		 	img = "",
+		 	imagenes = $('img'),
+			mismoTurno = false; 
+		
+		
+		if (imagenes.length){
+			if(imagenes.length == this.cantidadDados)
+				mismoTurno = true;
+			else
+				mismoTurno = false;
+		} else
+			mismoTurno = false;
+				
+		if (mismoTurno)
+			imagenes.each(function(){
+				d = Math.floor((Math.random() * 6) + 1);
+				this.src = "images/dado"+d+".png";
+			});
+		else{
+			for (var i = 0; i < this.cantidadDados; i++) {
+				d = Math.floor((Math.random() * 6) + 1);
+				img += "<img src='images/dado"+d+".png' />";
+			}
+			$('.dados').html(img);
 		}
+		
 	}
 
 }
@@ -30,10 +52,10 @@ $(function(){
 
 	$('input').blur(function(){
 		tirador.init(this.value);
-	})
+	});
 	
 	$("#jugar").click(function(){
-			tirador.tirarDados();
+		tirador.tirarDados();
 	});
 
 
