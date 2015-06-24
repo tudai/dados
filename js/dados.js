1c7381b759379d008c94e4410f3a33c671760398
Leonardo Navarro <maikndawer@gmail.com>
2015-06-24 17:23:23 -0300

    k

--- js/dados.js
+++ js/dados.js
@@ -51,15 +51,43 @@ var CubileteMagico = function(){
 			}
 			$('.dados').html(img_tag);
 		}
-		console.log(suma);
+		return suma;
 	}
 
+};
+
+var Regulador = function(){
+	
+	this.turnoActual = 1;
+	
+	this.cambiarDeTurno = function(){
+		if (this.turnoActual == 1){
+			$('input').attr('disabled', 'disabled');
+			this.turnoActual = 2;
+		}else{
+			this.turnoActual = 1;
+			$('input').removeAttr('disabled');
+		}	
+	};
+	
+	this.asignarPuntajes = function(puntaje){
+		var td_element = '<td>'+puntaje+'</td>';
+		
+		if (this.turnoActual == 1){
+			$('tbody').html("<tr>"+ td_element+"</tr>")
+		} else{
+			$('tr').last().append(td_element);
+		}
+			
+			
+	}
 }
+
 
 $(function(){
 	
 	var tirador = new CubileteMagico();
-	
+	var admin = new Regulador();
 	
 
 	$('input').blur(function(){
@@ -67,7 +95,9 @@ $(function(){
 	});
 	
 	$("#jugar").click(function(){
-		tirador.tirarDados();
+		var resultado = tirador.tirarDados();
+		admin.asignarPuntajes(resultado);
+		admin.cambiarDeTurno();
 	});
 
 
